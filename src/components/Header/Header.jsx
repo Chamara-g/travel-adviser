4ee36c80201f3e68ca201a9ce85cd37048def75a
chamara-g <chamara95.eng@gmail.com>
2021-08-10 20:01:08 +0530

    added component files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons';
+import { classes } from 'istanbul-lib-coverage';
 
 const Header = () => {
-    return(
-        <h1>Header</h1>
-    );
-}
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
