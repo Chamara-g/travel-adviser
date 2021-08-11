c1b579fb01829a13d6e9b66b1327f885940864ee
chamara-g <chamara95.eng@gmail.com>
2021-08-11 16:00:47 +0530

    api call added

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const URL =
+  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const options = {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        'x-rapidapi-key': 'de5086f1a7mshb145c1c726226e5p198c6ajsn7df76104ef91',
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    };
+
+    const {
+      data: { data },
+    } = await axios.get(URL, options);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
